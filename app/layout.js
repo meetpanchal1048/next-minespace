6de2153562626e3770e9b59d75546ab06becd74f
meetpanchal1048 <118661561+meetpanchal1048@users.noreply.github.com>
2024-07-24 07:22:10 +0530

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-V6473FKC3W"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'G-V6473FKC3W');
+        </script>
+      </head>
       <body className={styles.mainBody}>{children}</body>
     </html>
   );
